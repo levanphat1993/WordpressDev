69b2abd5a4eaa5e3a676551dafb4a12e20344abf
levanphat1993 <levanphatit1993@gmail.com>
2022-05-04 21:51:08 +0700

    add adv theme support

---
+++ wp-content/themes/wpThemes/functions.php
@@ -0,0 +1,16 @@
+<?php
+	// Theme Support
+function adv_theme_support(){
+	
+
+	
+    // Nav Menus
+	register_nav_menus(array(
+			'primary' => __('Primary Menu'),
+			'footer'  => __('Footer Menu')
+	));	
+	
+ 
+}
+	
+add_action('after_setup_theme', 'adv_theme_support');
